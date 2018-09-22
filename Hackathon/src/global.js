fc1d2acd8c22e4deffd5bc46d268e99cf400a982
Bingolina <43003241+Bingolina@users.noreply.github.com>
2018-09-22 15:40:04 +1000

    update global

--- Hackathon/src/global.js
+++ Hackathon/src/global.js
@@ -1,12 +1,21 @@
 global.storeData = [
-  {avatar: "HQ", title: "Rosebery", rating: 100, description: "This is a description", distance: 4.2, price: 50},
-  {avatar: "DM", title: "Zetland", rating: 5, description: "This is a description x2", distance: 4.1, price: 50},
-  {avatar: "JR", title: "Eastlake", rating: 8, description: "This is another description", distance: 2.1, price: 50},
-  {avatar: "MO", title: "Kensington", rating: 10, description: "This is not a description", distance: 1.3, price: 50},
-  {avatar: "JE", title: "Kensington", rating: 9, description: "What am I?", distance: 1.3, price: 60},
-  {avatar: "JL", title: "Kensington", rating: 7, description: "React Native is awesome", distance: 1.3, price: 70},
-  {avatar: "JW", title: "Kensington", rating: 9, description: "This is a description", distance: 1.3, price: 80},
-  {avatar: "JM", title: "Randwick", rating: 9, description: "This is a description x7", distance: 1, price: 50},
-  {avatar: "AU", title: "Kingsford", rating: 10, description: "This is a description x99", distance: 4, price: 50},
-  {avatar: "HO", title: "Waterloo", rating: 1, description: "This is a description", distance: 3.1, price: 50},
+  {avatar: "HQ", title: "Rosebery", rating: "⭐️10", description: "This is a description", distance: 4.2, price: 50},
+  {avatar: "DM", title: "Zetland", rating: "⭐️5", description: "This is a description x2", distance: 4.1, price: 50},
+  {avatar: "JR", title: "Eastlake", rating: "⭐️8", description: "This is another description", distance: 2.1, price: 50},
+  {avatar: "MO", title: "Kensington", rating: "⭐️10", description: "This is not a description", distance: 1.3, price: 50},
+  {avatar: "JE", title: "Kensington", rating: "⭐️9", description: "What am I?", distance: 1.3, price: 60},
+  {avatar: "JL", title: "Kensington", rating: "⭐️7", description: "React Native is awesome", distance: 1.3, price: 70},
+  {avatar: "JW", title: "Kensington", rating: "⭐️9", description: "This is a description", distance: 1.3, price: 80},
+  {avatar: "JM", title: "Randwick", rating: "⭐️9", description: "This is a description x7", distance: 1, price: 50},
+  {avatar: "AU", title: "Kingsford", rating: "⭐️10", description: "This is a description x99", distance: 4, price: 50},
+  {avatar: "HO", title: "Waterloo", rating: "⭐️1", description: "This is a description", distance: 3.1, price: 50},
+]
+
+global.detailData = {
+  moreDescription: "It’s in kingsford in a super clean and homey flat which is easy to get to the beach or city from. It's just walking distance to UNSW, the hospital, kingsford junction and The Spot randwick which have nice restaurants and bars. ", location: "barker st. kensington", availbleTime: "10 Oct to 1 Jan"；
+}
+
+global.reviews = [{name: "Andreas", comment:"Nice clean room."},
+  {name: "Michael", comment:"Nice person."},
+
 ]
