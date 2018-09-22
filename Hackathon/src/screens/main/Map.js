5b418312595330bf3325cc1d313fb2244d4f7ee9
Yiheng <development.henryquan@gmail.com>
2018-09-22 16:43:01 +1000

    cover up map, lol

--- Hackathon/src/screens/main/Map.js
+++ Hackathon/src/screens/main/Map.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text, WebView, StyleSheet } from 'react-native';
+import { QuickTouchable } from '../../component';
+import { Actions } from 'react-native-router-flux';
 
 class Map extends Component {
   render() {
-    const { container } = styles;
+    const { container, cover, bottom } = styles;
     return (
       <View style={container}>
         <WebView source={{uri: 'https://www.google.com.au/maps/'}}/>
+        <View style={cover}>
+
+        </View>
+        <View style={bottom}>
+          <QuickTouchable onPress={() => Actions.nearby()}>
+            <Text>Around me</Text>
+          </QuickTouchable>
+        </View>
       </View>
     )
   };
@@ -15,6 +25,17 @@ class Map extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  cover: {
+    position: 'absolute', justifyContent: 'center',
+    height: 57, width: '100%',
+    backgroundColor: 'white'
+  },
+  bottom: {
+    position: 'absolute', justifyContent: 'flex-end', alignItems: 'center',
+    
+    height: 50, width: '100%',
+    backgroundColor: 'white', bottom: 0,
   }
 });
 
