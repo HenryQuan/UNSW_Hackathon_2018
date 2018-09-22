adb7239191d27586af19b2557fb82078186e8f43
Yiheng <development.henryquan@gmail.com>
2018-09-22 14:48:38 +1000

    update component index

--- Hackathon/src/component/index.js
+++ Hackathon/src/component/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export * from './common/FormBox';
-=======
 export * from './common/FormBox';
 export * from './common/QuickTouchable';
 export * from './list/StoreList';
->>>>>>> list
