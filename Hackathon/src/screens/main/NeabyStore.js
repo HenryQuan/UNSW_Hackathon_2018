b22eae5715516ffc3886865b3b214f5ef7b76c4b
Bingolina <43003241+Bingolina@users.noreply.github.com>
2018-09-22 13:53:26 +1000

    update neabystore

--- Hackathon/src/screens/main/NeabyStore.js
+++ Hackathon/src/screens/main/NeabyStore.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { StoreList } from '../../component/list/StoreList';
-
+import { StoreListCell } from '../../component/list/StoreListCell';
 class NeabyStore extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <StoreList />
+        <StoreListCell />
       </View>
     );
   }
